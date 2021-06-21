1595dc59d03c2c59b26d694befd94471df2e3661
Ramkumar <spramkumar89@users.noreply.github.com>
2021-06-22 00:01:54 +0530

    Basic gray color base website design is completed

--- pages/home.js
+++ pages/home.js
@@ -3,7 +3,7 @@ import { Fragment } from "react";
 import { Disclosure, Menu, Transition } from "@headlessui/react";
 import { BellIcon, MenuIcon, XIcon } from "@heroicons/react/outline";
 
-const navigation = ["Dashboard", "Team", "Projects", "Calendar", "Reports"];
+const navigation = ["Home", "Transaction", "Features", "Budget", "Reports"];
 const profile = ["Your Profile", "Settings", "Sign out"];
 
 function classNames(...classes) {
@@ -13,7 +13,7 @@ function classNames(...classes) {
 export default function home() {
   return (
     <div>
-      <Disclosure as="nav" className="bg-gray-800">
+      <Disclosure as="nav" className="bg-gray-50">
         {({ open }) => (
           <>
             <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
@@ -34,7 +34,7 @@ export default function home() {
                             {/* Current: "bg-gray-900 text-white", Default: "text-gray-300 hover:bg-gray-700 hover:text-white" */}
                             <a
                               href="#"
-                              className="bg-gray-900 text-white px-3 py-2 rounded-md text-sm font-medium"
+                              className="bg-gray-600 text-white px-3 py-2 rounded-md text-sm font-medium"
                             >
                               {item}
                             </a>
@@ -43,7 +43,7 @@ export default function home() {
                           <a
                             key={item}
                             href="#"
-                            className="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium"
+                            className="text-gray-00 hover:bg-gray-600 hover:text-white px-3 py-2 rounded-md text-sm font-medium"
                           >
                             {item}
                           </a>
@@ -85,7 +85,7 @@ export default function home() {
                           >
                             <Menu.Items
                               static
-                              className="origin-top-right absolute right-0 mt-2 w-48 rounded-md shadow-lg py-1 bg-white ring-1 ring-black ring-opacity-5 focus:outline-none"
+                              className="origin-top-right absolute right-0 mt-2 w-48 rounded-md shadow-sm py-1 bg-white ring-1 ring-black ring-opacity-5 focus:outline-none"
                             >
                               {profile.map((item) => (
                                 <Menu.Item key={item}>
@@ -93,7 +93,7 @@ export default function home() {
                                     <a
                                       href="#"
                                       className={classNames(
-                                        active ? "bg-gray-100" : "",
+                                        active ? "bg-gray-50" : "",
                                         "block px-4 py-2 text-sm text-gray-700"
                                       )}
                                     >
@@ -185,17 +185,151 @@ export default function home() {
           </>
         )}
       </Disclosure>
-
-      <header className="bg-white shadow">
-        <div className="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8">
-          <h1 className="text-3xl font-bold text-gray-900">Dashboard</h1>
+      {/*
+      <header className="bg-gray-700 text-yellow-500 shadow">
+        <div className="max-w-7xl mx-auto p-4 sm:px-6 lg:px-8">
+          <h3 className="text-xl font-bold">LOTUS MONEY</h3>
         </div>
       </header>
-      <main>
-        <div className="max-w-7xl mx-auto py-6 sm:px-6 lg:px-8">
+      */}
+      <main class="bg-gray-50">
+        <div className="max-w-7xl mx-auto py-2 sm:px-6 lg:px-8">
           {/* Replace with your content */}
-          <div className="px-4 py-6 sm:px-0">
-            <div className="border-4 border-dashed border-gray-200 rounded-lg h-96" />
+          <div class="flex flex-row">
+            <div class="flex-none w-1/4 hidden md:block m-2">
+              <div class="grid grid-flow-row gap-4">
+                <div class="bg-gray-100 rounded-lg shadow-md p-2">
+                  <div id="header">
+                    <div id="header-text" class="leading-5 sm text-gray-500">
+                      <h4 id="name" class="text-xl font-semibold">
+                        John Doe
+                      </h4>
+                    </div>
+                  </div>
+                  <div id="quote">
+                    <q class="italic text-gray-500">
+                      Lorem ipsum dolor sit amet, consectetur adipiscing elit,
+                      sed do eiusmod tempor incididunt ut labore et dolore magna
+                      aliqua.
+                    </q>
+                  </div>
+                </div>
+                <div class="bg-gray-100 rounded-lg shadow-md p-2">
+                  <div id="header">
+                    <div id="header-text" class="leading-5 sm text-gray-500">
+                      <h4 id="name" class="text-xl font-semibold">
+                        John Doe
+                      </h4>
+                    </div>
+                  </div>
+                  <div id="quote">
+                    <q class="italic text-gray-500">
+                      Lorem ipsum dolor sit amet, consectetur adipiscing elit,
+                      sed do eiusmod tempor incididunt ut labore et dolore magna
+                      aliqua.
+                    </q>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div class="flex-auto p-2">
+              <div class="grid grid-flow-row gap-4">
+                <div class="bg-gray-100 rounded-lg shadow-md p-2">
+                  <div id="header">
+                    <div id="header-text" class="leading-5 sm text-gray-500">
+                      <h4 id="name" class="text-xl font-semibold">
+                        John Doe
+                      </h4>
+                    </div>
+                  </div>
+                  <div id="quote">
+                    <q class="italic text-gray-500">
+                      Lorem ipsum dolor sit amet, consectetur adipiscing elit,
+                      sed do eiusmod tempor incididunt ut labore et dolore magna
+                      aliqua.Lorem ipsum dolor sit amet, consectetur adipiscing
+                      elit, sed do eiusmod tempor incididunt ut labore et dolore
+                      magna aliqua.Lorem ipsum dolor sit amet, consectetur
+                      adipiscing elit, sed do eiusmod tempor incididunt ut
+                      labore et dolore magna aliqua.Lorem ipsum dolor sit amet,
+                      consectetur adipiscing elit, sed do eiusmod tempor
+                      incididunt ut labore et dolore magna aliqua.Lorem ipsum
+                      dolor sit amet, consectetur adipiscing elit, sed do
+                      eiusmod tempor incididunt ut labore et dolore magna
+                      aliqua.Lorem ipsum dolor sit amet, consectetur adipiscing
+                      elit, sed do eiusmod tempor incididunt ut labore et dolore
+                      magna aliqua.Lorem ipsum dolor sit amet, consectetur
+                      adipiscing elit, sed do eiusmod tempor incididunt ut
+                      labore et dolore magna aliqua.
+                    </q>
+                  </div>
+                </div>
+                <div class="bg-gray-100 rounded-lg shadow-md p-2">
+                  <div id="header">
+                    <div id="header-text" class="leading-5 sm text-gray-500">
+                      <h4 id="name" class="text-xl font-semibold">
+                        John Doe
+                      </h4>
+                    </div>
+                  </div>
+                  <div id="quote">
+                    <q class="italic text-gray-500">
+                      Lorem ipsum dolor sit amet, consectetur adipiscing elit,
+                      sed do eiusmod tempor incididunt ut labore et dolore magna
+                      aliqua.Lorem ipsum dolor sit amet, consectetur adipiscing
+                      elit, sed do eiusmod tempor incididunt ut labore et dolore
+                      magna aliqua.Lorem ipsum dolor sit amet, consectetur
+                      adipiscing elit, sed do eiusmod tempor incididunt ut
+                      labore et dolore magna aliqua.Lorem ipsum dolor sit amet,
+                      consectetur adipiscing elit, sed do eiusmod tempor
+                      incididunt ut labore et dolore magna aliqua.Lorem ipsum
+                      dolor sit amet, consectetur adipiscing elit, sed do
+                      eiusmod tempor incididunt ut labore et dolore magna
+                      aliqua.Lorem ipsum dolor sit amet, consectetur adipiscing
+                      elit, sed do eiusmod tempor incididunt ut labore et dolore
+                      magna aliqua.Lorem ipsum dolor sit amet, consectetur
+                      adipiscing elit, sed do eiusmod tempor incididunt ut
+                      labore et dolore magna aliqua.Lorem ipsum dolor sit amet,
+                      consectetur adipiscing elit, sed do eiusmod tempor
+                      incididunt ut labore et dolore magna aliqua.Lorem ipsum
+                      dolor sit amet, consectetur adipiscing elit, sed do
+                      eiusmod tempor incididunt ut labore et dolore magna
+                      aliqua.Lorem ipsum dolor sit amet, consectetur adipiscing
+                      elit, sed do eiusmod tempor incididunt ut labore et dolore
+                      magna aliqua.Lorem ipsum dolor sit amet, consectetur
+                      adipiscing elit, sed do eiusmod tempor incididunt ut
+                      labore et dolore magna aliqua.Lorem ipsum dolor sit amet,
+                      consectetur adipiscing elit, sed do eiusmod tempor
+                      incididunt ut labore et dolore magna aliqua.Lorem ipsum
+                      dolor sit amet, consectetur adipiscing elit, sed do
+                      eiusmod tempor incididunt ut labore et dolore magna
+                      aliqua.Lorem ipsum dolor sit amet, consectetur adipiscing
+                      elit, sed do eiusmod tempor incididunt ut labore et dolore
+                      magna aliqua.
+                    </q>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div class="flex-none w-1/4 hidden md:block p-2">
+              <div class="grid grid-flow-row gap-4">
+                <div class="bg-gray-100 rounded-lg shadow-md p-2">
+                  <div id="header">
+                    <div id="header-text" class="leading-5 sm text-gray-500">
+                      <h4 id="name" class="text-xl font-semibold">
+                        John Doe
+                      </h4>
+                    </div>
+                  </div>
+                  <div id="quote">
+                    <q class="italic text-gray-500">
+                      Lorem ipsum dolor sit amet, consectetur adipiscing elit,
+                      sed do eiusmod tempor incididunt ut labore et dolore magna
+                      aliqua.
+                    </q>
+                  </div>
+                </div>
+              </div>
+            </div>
           </div>
           {/* /End replace */}
         </div>
