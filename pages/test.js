f55e33442c5e256c6732abe7c51a9ba870976c32
Ramkumar <spramkumar89@users.noreply.github.com>
2021-07-27 11:21:04 +0530

    Updated home page components to load data based on the selected month

--- pages/test.js
+++ pages/test.js
@@ -1,4 +1,19 @@
-let date = "2021-07-01";
+/*incomecategoryValues : [{},{},{"Solo App Development":5},{},{}]
+expensecategoryValues : [{"Grocery":501},{"Shopping":403},{"Miscellaneous":2},{}]
+categoryValues : [{"Grocery":501},{"Shopping":403},{"Miscellaneous":2},{}]*/
 
-let arr = date.split("-");
-console.log(arr[1] + "-" + arr[0]);
+let incomecategoryValues = [{},{},{"Solo App Development":5},{},{}];
+let expensecategoryValues = [{"Grocery":501},{"Shopping":403},{"Miscellaneous":2},{}];
+let categoryValues = [];
+incomecategoryValues.forEach((item,index)=>{
+	console.log(`Item length ${item.size} and the item value is ${JSON.stringify(item)}`);
+	if(Object.keys(item).length !== 0){
+		categoryValues.push(item);
+	}
+});
+expensecategoryValues.forEach((item,index)=>{
+	if(Object.keys(item).length !== 0){
+		categoryValues.push(item);
+	}
+});
+console.log("CategoryValues : " + JSON.stringify(categoryValues));
